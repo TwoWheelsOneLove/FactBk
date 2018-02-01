d8f846855369253198fedc11776b047303967270
TwoWheelsOneLove <up795987@myport.ac.uk>
2018-02-01 13:46:00 +0000

    Started on log in page
    Login Page html started.

--- sql_initialise.sql
+++ sql_initialise.sql
@@ -4,10 +4,14 @@ create database if not exists factBk;
 
 create table if not exists factBk.fact (
 
-
 );
+
+
 
 create table if not exists factBk.user (
-
-
+  id int primary key auto_increment,
+  username varchar(20),
+  password varchar(20)
 );
+
+insert ignore into factBk.user values(1, 'DanLincoln', 'testPassword123');
