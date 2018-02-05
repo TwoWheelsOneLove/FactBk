23e6a889bc67f2ed6c613f7dbabf59002420b3f7
Nasser333 <33520475+Nasser333@users.noreply.github.com>
2018-02-05 18:18:51 +0000

    Update sql_initialise.sql

--- sql_initialise.sql
+++ sql_initialise.sql
@@ -3,9 +3,28 @@
 create database if not exists factBk;
 
 create table if not exists factBk.fact (
-
-
+  
+create TABLE user(
+ID INT(10) PRIMARY KEY,
+Email VARCHAR(30) NOT NULL,
+Password VARCHAR(50) NOT NULL,
 );
+
+
+create TABLE location(
+locationID INT(10) PRIMARY KEY,
+locationX FLOAT(3,10) NOT NULL,
+locationY FLOAT(3,10) NOT NULL,
+LocationReference TEXT(50) NOT NULL,
+);
+
+
+create TABLE facts(
+factID INT(10) PRIMARY KEY,
+factText TEXT(5000) NOT NULL,
+CONSTRAINT FOREIGN KEY (locationID)
+);
+
 
 create table if not exists factBk.user (
 
