fd5072d229cffb3833db22d657bf669e9bc9e50c
MadScientistIgor <thisisshift@gmail.com>
2018-02-05 18:42:31 +0000

    Update sql_initialise.sql

--- sql_initialise.sql
+++ sql_initialise.sql
@@ -9,7 +9,12 @@ email VARCHAR(30) NOT NULL,
 password VARCHAR(50) NOT NULL
 );
 
-
+create TABLE if not exists factBk.location(
+locationID INT(10) PRIMARY KEY,
+locationX FLOAT(3,10) NOT NULL,
+locationY FLOAT(3,10) NOT NULL,
+imgReference TEXT(50) NOT NULL
+);
 
 
 
