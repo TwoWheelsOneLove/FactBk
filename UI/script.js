6daf8deb5f7823c49f0694946ece9593f99a95b8
Isa-Muhammad <36093501+Isa-Muhammad@users.noreply.github.com>
2018-02-22 14:43:59 +0000

    Update script.js

--- UI/script.js
+++ UI/script.js
@@ -1,39 +1,89 @@
-    let p = document.getElementsByClassName('item1')[0]
-    let x = document.getElementById("myDIV");
-    let btn = document.getElementById('btn')
-    let item = document.getElementById('here').addEventListener('click',() =>{
-    let x = 1
-    console.log(p)
-      p.style.display = "none";
-  })
+let p = document.getElementsByClassName('item1')[0]
+let x = document.getElementById("myDIV");
+let y = document.getElementById("nav");
+let z = document.getElementById("myDIV2");
+let btn = document.getElementById("btn");
+let snd = document.getElementById("send");
+let cls = document.getElementById("close");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById('btn3');
+let item = document.getElementById('here').addEventListener('click',() =>{
+let x = 1
+  p.style.display = "none";
+})
+  y.style.display = "none";
 
-  function myFunction() {
-      if (x.style.display ==="block") {
-          x.style.display = "none";
-        }
-      else {
-          x.style.display = "block";
-      }
-  }
-
-  btn.addEventListener('click',() =>{
-    if (p.style.display ==="block") {
-        p.style.display = "none";
-      }
-    else {
-        p.style.display = "block";
-        x.style.display = 'none'
+function myFunction() {
+  if (x.style.display ==="block") {
+      x.style.display = "none";
     }
+  else {
+      x.style.display = "block";
+  }
+}
 
-  })
+btn.addEventListener('click',() =>{
+if (p.style.display ==="block") {
+    p.style.display = "none";
+  }
+else {
+    p.style.display = "block";
+    x.style.display = 'none'
+}
 
-/*Change between adding and removing the responsive class to the bottombar
-when the user clicks the icon*/ 
-   function myFunction2(){
-     let x = document.getElementById("factbkBar");
-     if (x.className === "bottombar"){
-         x.className += "responsive";
-     }else {
-       x.className = "bottombar";
-     }
-   }
+})
+
+btn2.addEventListener('click',() =>{
+if (y.style.display ==="block") {
+    y.style.display = "none";
+  }
+else {
+    y.style.display = "block";
+    p.style.display = 'none'
+}
+})
+
+btn3.addEventListener('click',() =>{
+if (p.style.display ==="block") {
+    p.style.display = "none";
+  }
+else {
+    p.style.display = "block";
+    y.style.display = 'none'
+    z.style.display = "none"
+}
+
+})
+
+
+snd.addEventListener('click',() =>{
+if (z.style.display ==="block") {
+    z.style.display = "none";
+  }
+else {
+    z.style.display = "none"
+}
+
+})
+
+
+cls.addEventListener('click',() =>{
+if (z.style.display ==="block") {
+    z.style.display = "none";
+  }
+else {
+    z.style.display = "none"
+}
+
+})
+
+
+function myFunction2() {
+let x = document.getElementById("myDIV2");
+if (x.style.display ==="block") {
+    x.style.display = "none";
+  }
+else {
+    x.style.display = "block";
+}
+}
