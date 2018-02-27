329d8dd205e6f2d2834c32cbb7453736f88f5e4c
Isa-Muhammad <36093501+Isa-Muhammad@users.noreply.github.com>
2018-02-27 22:59:07 +0000

    Update script.js

--- UI/script.js
+++ UI/script.js
@@ -1,130 +1,130 @@
-let p = document.getElementsByClassName('item1')[0]
-let x = document.getElementById("myDIV");
-let y = document.getElementById("nav");
-let z = document.getElementById("myDIV2");
-let a = document.getElementById("addFact");
-let btn = document.getElementById("btn");
+let main = document.getElementsByClassName('main')[0]
+let accForm = document.getElementById("accountForm");
+let nav = document.getElementById("navigation");
+let fForm = document.getElementById("feedbackForm");
+let factForm = document.getElementById("addFact");
+let SignInlnk = document.getElementById("SignInlnk");
 let snd = document.getElementById("send");
 let submit = document.getElementById("submitFact");
-let cls = document.getElementById("close");
-let cls2 = document.getElementById("close2");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById('btn3');
-let item = document.getElementById('here').addEventListener('click',() =>{
-let x = 1
-  p.style.display = "none";
+let clsFForm = document.getElementById("closeFForm");
+let clsFactForm = document.getElementById("closeFactForm");
+let signInbtn = document.getElementById("signInbtn");
+let logOutbtn = document.getElementById('logOutbtn');
+let signUplnk = document.getElementById('signUplnk').addEventListener('click',() =>{
+let accForm = 1
+  main.style.display = "none";
 })
-  y.style.display = "none";
+  nav.style.display = "none";
 
-function myFunction() {
-  if (x.style.display ==="block") {
-      x.style.display = "none";
+function switchToAccForm() {
+  if (accForm.style.display ==="block") {
+      accForm.style.display = "none";
     }
   else {
-      x.style.display = "block";
+      accForm.style.display = "block";
   }
 }
 
-btn.addEventListener('click',() =>{
-if (p.style.display ==="block") {
-    p.style.display = "none";
+SignInlnk.addEventListener('click',() =>{
+if (main.style.display ==="block") {
+    main.style.display = "none";
   }
 else {
-    p.style.display = "block";
-    x.style.display = 'none'
+    main.style.display = "block";
+    accForm.style.display = 'none'
 }
 
 })
 
-btn2.addEventListener('click',() =>{
-if (y.style.display ==="block") {
-    y.style.display = "none";
+signInbtn.addEventListener('click',() =>{
+if (nav.style.display ==="block") {
+    nav.style.display = "none";
   }
 else {
-    y.style.display = "block";
-    p.style.display = 'none'
+    nav.style.display = "block";
+    main.style.display = 'none'
 }
 })
 
-btn3.addEventListener('click',() =>{
-if (p.style.display ==="block") {
-    p.style.display = "none";
+logOutbtn.addEventListener('click',() =>{
+if (main.style.display ==="block") {
+    main.style.display = "none";
   }
 else {
-    p.style.display = "block";
-    y.style.display = 'none'
-    z.style.display = "none"
-    a.style.display = "none";
+    main.style.display = "block";
+    nav.style.display = 'none'
+    fForm.style.display = "none"
+    factForm.style.display = "none";
 }
 
 })
 
 
 snd.addEventListener('click',() =>{
-if (z.style.display ==="block") {
-    z.style.display = "none";
+if (fForm.style.display ==="block") {
+    fForm.style.display = "none";
   }
 else {
-    z.style.display = "none"
+    fForm.style.display = "none"
 }
 
 })
 
 
 submit.addEventListener('click',() =>{
-if (a.style.display ==="block") {
-    a.style.display = "none";
+if (factForm.style.display ==="block") {
+    factForm.style.display = "none";
   }
 else {
-    a.style.display = "none"
+    factForm.style.display = "none"
 }
 
 })
 
 
 
-cls.addEventListener('click',() =>{
-if (z.style.display ==="block") {
-    z.style.display = "none";
+clsFForm.addEventListener('click',() =>{
+if (fForm.style.display ==="block") {
+    fForm.style.display = "none";
   }
 else {
-    z.style.display = "none"
+    fForm.style.display = "none"
 }
 
 })
 
 
-cls2.addEventListener('click',() =>{
-if (a.style.display ==="block") {
-    a.style.display = "none";
+clsFactForm.addEventListener('click',() =>{
+if (factForm.style.display ==="block") {
+    factForm.style.display = "none";
   }
 else {
-    a.style.display = "none"
+    factForm.style.display = "none"
 }
 
 })
 
 
-function myFunction2() {
-let x = document.getElementById("myDIV2");
-if (x.style.display ==="block") {
-    x.style.display = "none";
+function displayFdBckForm() {
+let accForm = document.getElementById("feedbackForm");
+if (accForm.style.display ==="block") {
+    accForm.style.display = "none";
   }
 else {
-    x.style.display = "block";
-    a.style.display = "none";
+    accForm.style.display = "block";
+    factForm.style.display = "none";
 
 }
 }
 
 
-function myFunction3() {
-let a = document.getElementById("addFact");
-if (a.style.display ==="block") {
-    a.style.display = "none";
+function displayAddFctForm() {
+let factForm = document.getElementById("addFact");
+if (factForm.style.display ==="block") {
+    factForm.style.display = "none";
   }
 else {
-    a.style.display = "block";
-    z.style.display="none";
+    factForm.style.display = "block";
+    fForm.style.display="none";
 }
 }
